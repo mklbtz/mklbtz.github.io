3840edeb05c0fa459dea7ef1f2bd97378a567eec
mklbtz <mklbtz@gmail.com>
2014-09-27 22:58:21 -0400

    show some love

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+var _StatHat = _StatHat || [];
+_StatHat.push(['_setUser', 'Mzc5NyD9LTBB4AKD3yVSH_HZRusY']);
+(function() {
+        var sh = document.createElement('script'); sh.type = 'text/javascript';
+        sh.async = true;
+        sh.src = '//www.stathat.com/javascripts/api.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(sh, s);
+})();
+
+function heart () {
+  _StatHat.push(['_trackValue', 'XXXXXXXXXX', 3.14159]);
+}
